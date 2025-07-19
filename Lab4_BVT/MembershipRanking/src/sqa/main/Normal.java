abdeebfae9372c90a81d6c656952171ebd9b6118
Yuu <lbaskunglch@gmail.com>
2025-07-19 14:33:55 +0700

    แก้ให้ถูกมากขึ้น

--- Lab4_BVT/MembershipRanking/src/sqa/main/Normal.java
+++ Lab4_BVT/MembershipRanking/src/sqa/main/Normal.java
@@ -13,17 +13,17 @@ class Normal {
 	@CsvSource({
 	    "0, 15, 500, Standard",
 	    "1, 15, 500, Standard",
-	    "50000, 15, 500, Gold",
-	    "99999, 15, 500, Gold",
-	    "100000, 15, 500, Gold",
+	    "50000, 15, 500, Standard",
+	    "99999, 15, 500, Standard",
+	    "100000, 15, 500, Standard",
 	    "50000, 0, 500, Standard",
-	    "50000, 1, 500, Silver",
-	    "50000, 29, 500, Gold",
-	    "50000, 30, 500, Gold",
+	    "50000, 1, 500, Standard",
+	    "50000, 29, 500, Standard",
+	    "50000, 30, 500, Standard",
 	    "50000, 15, 0, Standard",
 	    "50000, 15, 1, Standard",
-	    "50000, 15, 999, Gold",
-	    "50000, 15, 1000, Gold"
+	    "50000, 15, 999, Standard",
+	    "50000, 15, 1000, Standard"
 	    /* test
 	    "50000,2,500,Silver",
 	    "50000,5,500,Gold",
